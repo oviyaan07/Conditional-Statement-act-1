1c12d9aa932a2dc8fa3ea2786d1a358382a22800
oviyaan07 <oviyaansingh04@gmail.com>
2024-11-03 12:23:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+num=int(input("Enter a number: "))
+if num>0:
+    print(f"{num} is a positive number")
+elif num==0:
+    print(f"{num} is equal to 0")    
+else:
+    print (f"{num} is a negetive number")  
